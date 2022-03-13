b34a8501e5727455d4c8d8828b1f13eae4650c66
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-03-13 20:43:49 +0100

    Specifikacija interfejsa MagacinInterfejs u okviru paketa magacin, dodate metode za dodavanje, izbacivanje i pronalazenje artikala

---
+++ Magacin/src/magacin/MagacinInterfejs.java
@@ -0,0 +1,11 @@
+package magacin;
+
+import model.Artikal;
+
+public interface MagacinInterfejs {
+	
+	public void dodajArtikal(Artikal artikal);
+	public void izbaciArtikal(Artikal artikal);
+	public Artikal pronadjiArtikal(int sifra);
+	
+}
