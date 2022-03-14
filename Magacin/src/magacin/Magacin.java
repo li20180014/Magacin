fa721cd11c25baa4d3a00f38b03b02069a1b9438
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-03-14 14:13:20 +0100

    Implementirane metode interfejsa MagacinInterfejs u okviru klase Magacin

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,24 +1,34 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import model.Artikal;
 
 public class Magacin implements MagacinInterfejs {
+	
+	List<Artikal> artikli = new LinkedList<>();
 
 	@Override
 	public void dodajArtikal(Artikal artikal) {
 		// TODO Auto-generated method stub
-
+		artikli.add(artikal);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
 		// TODO Auto-generated method stub
-
+		artikli.remove(artikal);
 	}
 
 	@Override
 	public Artikal pronadjiArtikal(int sifra) {
-		// TODO Auto-generated method stub
+		for (Artikal artikal : artikli) {
+			if (artikal.getSifra()==sifra) {
+				return artikal;
+			}
+		}
+		
 		return null;
 	}
 
