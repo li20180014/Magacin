fede0abb1cf79fade879f25c1042a75fe268009b
li20180014 <li20180014@student.fon.bg.ac.rs>
2022-03-14 14:30:45 +0100

    Druga implementacija metode dodajArtikal klase Magacin na master grani

--- Magacin/src/magacin/Magacin.java
+++ Magacin/src/magacin/Magacin.java
@@ -1,19 +1,24 @@
 package magacin;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import model.Artikal;
 
 public class Magacin implements MagacinInterfejs {
 
+	public List<Artikal> artikli= new LinkedList<Artikal>();
+	
 	@Override
 	public void dodajArtikal(Artikal artikal) {
 		// TODO Auto-generated method stub
-
+		artikli.add(0, artikal);
 	}
 
 	@Override
 	public void izbaciArtikal(Artikal artikal) {
 		// TODO Auto-generated method stub
-
+	
 	}
 
 	@Override
